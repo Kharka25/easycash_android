9f30f5933575d12bad7d78bd0d734b5894a45837
Kharka25 <adewoyindaniel6@gmail.com>
2025-08-09 21:27:04 +0100

    auth screens - WIP

--- app/src/main/java/com/example/easycash/MainActivity.java
+++ app/src/main/java/com/example/easycash/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.easycash;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.easycash.auth.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +19,7 @@ public class MainActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
         SplashScreen.installSplashScreen(this);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
